8d65b04db0622d247532e5362483a243af96d67f
leuchthelp <gfpinthehouse@gmail.com>
2026-05-09 16:27:20 +0200

    Begin database integration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(
-            tauri_plugin_log::Builder::new()
-                .level(tauri_plugin_log::log::LevelFilter::Info)
-                .build(),
-        )
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { invoke } from "@tauri-apps/api/core";
 import { SongItem } from "./MediaItems";
-import { warn, debug, trace, info, error } from "@tauri-apps/plugin-log";
 import Database from "@tauri-apps/plugin-sql";
 
 const db = await Database.load("sqlite:test.db");
+
+let res;
+let query =
+  "CREATE TABLE if NOT EXISTS sources (" +
+  "id INTEGER PRIMARY KEY," +
+  "name TEXT NOT NULL," +
+  "details TEXT" +
+  ");";
+res = await db.execute(query);
+
+res = await db.execute("DELETE FROM sources");
+
+res = await db.execute("INSERT into sources (name, details)\n VALUES ($1, $2)", [
+  "Laptop",
+  '{"brand": "Dell", "price": 1200, "features": ["i7", "16GB RAM", "512GB SSD"]}',
+]);
+
+res = await db.select(
+  "SELECT json_extract(details, '$.brand') AS brand\n" + "FROM sources;",
+);
+console.log(res)
+
+res = await db.select(
+  "SELECT json_extract(details, '$.features') AS features\n" + "FROM sources;",
+) as Array<Object>;
+console.log(res.at(0))
 
 let main_space = document.getElementById("main-space") as HTMLDivElement;
 let newSongItem = new SongItem();
