93abe0b99a468cdb5bcac9a3aa2ba9f6a6db3879
leuchthelp <gfpinthehouse@gmail.com>
2026-05-13 17:35:41 +0200

    Change over to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+  import { SongItem } from "./MediaItems";
+  import { MediaCache } from "./db/MediaCache.ts";
+
+  const cache = new MediaCache();
+
+  let test = [
+    "Laptop",
+    '{"brand": "Dell", "price": 1200, "features": ["i7", "16GB RAM", "512GB SSD"]}',
+  ];
+  let res = await cache.addEntries(test);
+  let res1 = await cache.getEntries("features");
+  console.log(res1);
+
+  let main_space = document.getElementById("main-space") as HTMLDivElement;
+  let newSongItem = new SongItem();
+
+  let styling = newSongItem.defaultStyling();
+
+  const newElement = document.createElement("div");
+
+  for (var style of styling) {
+    newElement.classList.add(style);
+  }
+  newElement.classList.add("ring");
+  newElement.classList.add(newSongItem.outlineGradient());
+
+  const spawn = [{ transform: "scale(0)" }, { transform: "scale(1)" }];
+  const spawnTime = {
+    duration: 1000,
+    iterations: 1,
+  };
+  newElement.animate(spawn, spawnTime);
+  main_space.appendChild(newElement);
+</script>
+
+<main class="flex h-screen w-screen flex-col place-items-center-safe">
+  <div id="main-space" class="mt-5 h-full"></div>
+  <div class="grid-play-options">
+    <button class="playbar-styled-button">1</button>
+    <button class="playbar-styled-button">2</button>
+    <button class="playbar-styled-button">3</button>
+  </div>
+</main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
+  plugins: [sveltekit(), tailwindcss()],
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
@@ -26,5 +29,4 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-  plugins: [tailwindcss()],
 }));
