2f9a4e5f44cd11dd2acf5d771d4789591c5ec0ca
leuchthelp <gfpinthehouse@gmail.com>
2026-05-15 23:25:38 +0200

    current

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,5 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,24 +4,20 @@
   import Playbar from "$lib/components/Playbar/Playbar.svelte";
   import SongItemComponent from "$lib/components/MediaItems/SongItemComponent.svelte";
 
-
   await db.deleteAll();
 
-
-  const tmp = new SongItem()
+  const tmp = new SongItem();
   const test = ["testItem", JSON.stringify(tmp)];
   console.log(tmp);
-  await db.addEntries(test)
-
+  await db.addEntries(test);
 
   let res1 = $state(await db.getEntries());
   $inspect(res1);
 
-
   function parseObject(item: any) {
-    const test = Object.assign(new SongItem(), JSON.parse(item["details"]))
+    const test = Object.assign(new SongItem(), JSON.parse(item["details"]));
 
-    console.log(test)
+    console.log(test);
     return test;
   }
 </script>
