3874d1c33548eda3f9bdafa8fdf4d2eb3c490e17
leuchthelp <gfpinthehouse@gmail.com>
2026-05-27 17:54:13 +0200

    add auth client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { sveltekitCookies } from "better-auth/svelte-kit";
+import { getRequestEvent } from "$app/server";
 import { db } from "./db/database";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, { 
-    provider: "sqlite", // or "pg" or "mysql"
-  }), 
-  //... the rest of your config
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+  }),
+  emailAndPassword: { 
+    enabled: true, 
+  },
+  plugins: [sveltekitCookies(getRequestEvent)],
 });
