e828fb840e7d6a36a4705814c20262cc70fd17cc
leuchthelp <gfpinthehouse@gmail.com>
2026-06-08 16:52:02 +0200

    fix content view

--- src/lib/components/MediaItems/ItemComponents/AlbumItemComponent.svelte
+++ src/lib/components/MediaItems/ItemComponents/AlbumItemComponent.svelte
@@ -8,9 +8,7 @@
 
   let { item }: Props = $props();
   let styling = $derived(item.defaultStyling + " " + item.outlineGradient);
-  let content = $derived(item.content.at(0));
-
-  $inspect(content);
+  let content = $derived(item.content.filter((item) => item.type === "Name").at(0));
 </script>
 
-<Button {styling}><a href="/pages/album/{item.uuid}">{content}</a></Button>
+<Button {styling}><a href="/pages/album/{item.uuid}">{content?.description}</a></Button>
