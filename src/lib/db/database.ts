1855e6360f683c289776938197a1971e0fd5bc80
leuchthelp <gfpinthehouse@gmail.com>
2026-06-13 11:25:24 +0200

    little cleanup

--- src/lib/db/database.ts
+++ src/lib/db/database.ts
@@ -1,8 +1,12 @@
 import { PGlite } from "@electric-sql/pglite";
 import { drizzle } from "drizzle-orm/pglite";
 import { relations } from "./relations.ts";
+import { live } from "@electric-sql/pglite/live";
 
-const client = new PGlite("idb://dev");
-const db = drizzle({ client: client, relations: relations });
+export const client = new PGlite({
+  dataDir: "idb://dev",
+  extensions: { live },
+});
+const db = drizzle({ client: client, relations: relations, jit: true });
 
 export { db };
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,20 +2,13 @@ export const ssr = false;
 
 import { providerDataQuery } from "$lib/db/queries.ts";
 import type { LayoutLoad } from "./$types.d.ts";
-import Database from "@tauri-apps/plugin-sql";
 import { migrate_pglite } from "../lib/db/migrate.ts";
 
 export const load: LayoutLoad = async () => {
-  // let tmp = await Database.load("sqlite:dev.db")
-  // await tmp.execute("DELETE FROM MediaItems")
-  // await tmp.execute("DELETE FROM ProviderItems")
-
-  migrate_pglite().then(() => {
-    console.log("migration success");
+  migrate_pglite().catch((e) => {
+    console.error("migration success", e);
   });
-
-  const res = await providerDataQuery.execute();
   return {
-    post: res,
+    post: await providerDataQuery.execute(),
   };
 };
