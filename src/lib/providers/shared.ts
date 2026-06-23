2c402ccd85ecafb4ffe4dedcf163ebf0edfaac7c
leuchthelp <gfpinthehouse@gmail.com>
2026-06-23 16:43:00 +0200

    refactor(shared): use isTauri()

--- src/lib/providers/shared.ts
+++ src/lib/providers/shared.ts
@@ -1,14 +1,12 @@
 import { v7 as uuidv7 } from "uuid";
+import { isTauri } from "@tauri-apps/api/core";
+import { arch, hostname, platform, version } from "@tauri-apps/plugin-os";
 
 async function tauriDevice() {
-  const { arch, hostname, platform, version } =
-    await import("@tauri-apps/plugin-os");
-
   return (await hostname()) + "-" + platform() + "-" + arch() + "-" + version();
 }
 
 const uuid = uuidv7();
-const device: string =
-  "__TAURI_INTERNALS__" in window ? await tauriDevice() : navigator.platform;
+const device: string = isTauri() ? await tauriDevice() : navigator.platform;
 
 export { uuid, device };
