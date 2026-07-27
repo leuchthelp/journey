e129bdfad243ec3d3619ef8368d20201835ad517
leuchthelp <gfpinthehouse@gmail.com>
2026-07-28 00:33:24 +0200

    fix: figure out clean enough solution to using await in svelte derive

--- src/lib/components/Settings/Auth/JellyfinAuth.svelte
+++ src/lib/components/Settings/Auth/JellyfinAuth.svelte
@@ -3,6 +3,20 @@
   import { strip } from "#lib/components/helpers.ts";
   import { API } from "#lib/proxy.ts";
 
+  const getProvider = async (key?: number): Promise<ProviderDTO | null> => {
+    if (!key) return null;
+
+    return await API.provider
+      .get_provider(key)
+      .then((response) => {
+        return strip(response);
+      })
+      .catch((err) => {
+        console.error(err);
+        return null;
+      });
+  };
+
   type Props = {
     key?: number;
   };
@@ -14,42 +28,33 @@
   let psw = $state("");
   let url = $state("");
 
-  let provider: ProviderDTO | null = $state(null);
-
-  $effect(() => {
-    if (key)
-      (async () => {
-        provider = await API.provider
-          .get_provider(key)
-          .then((response) => {
-            return strip(response);
-          })
-          .catch((err) => {
-            console.error(err);
-            return null;
-          });
-      })();
-  });
+  let provider = $derived(await getProvider(key));
+  $inspect(provider);
 </script>
 
 <div class="">
-  {#if success}
-    <div>Connected</div>
-    <div>{key}</div>
-    <div>{provider}</div>
-    <button onclick={() => console.debug("no impl")}>Remove Connection</button>
-  {:else}
-    <form>
-      <label for="url">Server Address</label>
-      <input type="url" required bind:value={url} />
+  {#await provider}
+    <div>Loading</div>
+  {:then provider}
+    {#if success}
+      <div>Connected</div>
+      <div>{key}</div>
+      <div>{provider}</div>
+      <button onclick={() => console.debug("no impl")}>Remove Connection</button
+      >
+    {:else}
+      <form>
+        <label for="url">Server Address</label>
+        <input type="url" required bind:value={url} />
 
-      <label for="uname">Username</label>
-      <input required bind:value={uname} />
+        <label for="uname">Username</label>
+        <input required bind:value={uname} />
 
-      <label for="psw">Password</label>
-      <input type="password" required bind:value={psw} />
+        <label for="psw">Password</label>
+        <input type="password" required bind:value={psw} />
 
-      <button onclick={async () => console.debug("no impl")}>Connect</button>
-    </form>
-  {/if}
+        <button onclick={async () => console.debug("no impl")}>Connect</button>
+      </form>
+    {/if}
+  {/await}
 </div>
